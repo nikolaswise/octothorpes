e158dd51729b6928aa05405d088d02a019ef816e
nikolaswise <nikolaswise@gmail.com>
2022-05-17 21:55:08 -0700

    compress all records across all graphs into a single node

--- src/routes/feed.json.js
+++ src/routes/feed.json.js
@@ -1,16 +1,50 @@
 let sources = [
   'https://nikolas.ws/octothorpes.json/'
 ]
+const thorpMap = new Map()
 
 const fetchFromSources = async () => {
   let promises = sources.map(async (source) => await fetch(source))
   let resolve = await Promise.all(promises)
   let json = await Promise.all(resolve.map(async (res) => res.json()))
 
-  let records = json.reduce((acc, val) => {
-    acc['@graph'] = [...acc['@graph'], ...val['@graph'].flat()]
-    return acc
-  }, {
+  // fake record
+  json = [...json, {'@graph': [{
+    uri: 'https://nikolas.ws/thinking-about-octothorpes',
+    "octothorpedBy": [
+      "https://octothorp.es/testing"
+    ]
+    }
+  ]}]
+
+  // oh god dont look
+  json.forEach(doc => {
+    doc['@graph'].forEach(node => {
+      let extantNode = thorpMap.get(node.uri)
+      if (extantNode) {
+        if (node.octothorpes) {
+          extantNode.octothorpes = node.octothorpes.length ? [...node.octothorpes] : node.octothorpes
+        }
+        if (node.octothorpedBy) {
+          extantNode.octothorpedBy = node.octothorpedBy.length ? [...node.octothorpedBy] : node.octothorpedBy
+        }
+        thorpMap.set(node.uri, extantNode)
+      } else {
+        let thorpNode = {
+          uri: node.uri
+        }
+        if (node.octothorpes) {
+          thorpNode.octothorpes = node.octothorpes.length ? [...node.octothorpes] : node.octothorpes
+        }
+        if (node.octothorpedBy) {
+          thorpNode.octothorpedBy = node.octothorpedBy.length ? [...node.octothorpedBy] : node.octothorpedBy
+        }
+        thorpMap.set(node.uri, thorpNode)
+      }
+    })
+  })
+
+  let records = {
     '@context': {
       "octothorpes": {
         "@id": "https://vocab.octothorp.es/#octothorpes",
@@ -22,8 +56,8 @@ const fetchFromSources = async () => {
         "@reverse": "octothorpes"
       },
     },
-    '@graph': []
-  })
+    '@graph': [...thorpMap.values()]
+  }
 
   return records
 }
